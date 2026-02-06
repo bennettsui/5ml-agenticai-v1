b61bf6138ee81dab326baf6fa60b11e0677027fd
angelika-5mileslab <141980615+angelika-5mileslab@users.noreply.github.com>
2026-02-06 19:27:19 +0800

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     unoptimized: true,
   },
